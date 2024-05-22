6eccfa175fa3a89b771a37116fb2d140bc28abfb
MatejCervenka <127132225+MatejCervenka@users.noreply.github.com>
2024-05-22 13:57:48 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ReserveSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
