29698f593bdf93e2a95fb7b711360c9ac11333a0
MatejCervenka <127132225+MatejCervenka@users.noreply.github.com>
2024-05-27 21:18:26 +0200

    Add: IndexPageViewModel.cs, html, css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ReserveSystem.Database;
 using ReserveSystem.Models;
+using ReserveSystem.ViewModels;
 
 namespace ReserveSystem.Controllers;
 
@@ -16,6 +17,59 @@ public class HomeController : Controller
     
     public IActionResult Index()
     {
-        return View();
+        var viewModel = new IndexPageViewModel
+        {
+            Login = new LoginViewModel
+            {
+                Id = 1,
+                Username = "exampleUser",
+                Password = "examplePass",
+                CreatedAt = DateTime.Now
+            },
+            Pricing = new PricingViewModel
+            {
+                Id = 1,
+                Title = "Basic Plan",
+                Description = "This is a basic plan.",
+                SaleAmount = 49.99M,
+                Amount = 59.99M,
+                Currency = 'K'
+            },
+            Reservation = new ReservationViewModel
+            {
+                Id = 1,
+                ServiceId = 1,
+                Name = "John",
+                Surname = "Doe",
+                Phone = "123-456-7890",
+                Email = "john.doe@example.com",
+                CreatedAt = DateTime.Now,
+                ServiceViewModel = new ServiceViewModel
+                {
+                    Id = 1,
+                    Name = "Haircut"
+                }
+            },
+            Service = new ServiceViewModel
+            {
+                Id = 1,
+                Name = "Haircut",
+                Reservations = new List<ReservationViewModel>
+                {
+                    new ReservationViewModel
+                    {
+                        Id = 1,
+                        ServiceId = 1,
+                        Name = "John",
+                        Surname = "Doe",
+                        Phone = "123-456-7890",
+                        Email = "john.doe@example.com",
+                        CreatedAt = DateTime.Now
+                    }
+                }
+            }
+        };
+
+        return View(viewModel);
     }
 }
---
+++ ViewModels/IndexPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ReserveSystem.ViewModels;
+
+public class IndexPageViewModel
+{
+    public LoginViewModel Login { get; set; }
+    public PricingViewModel Pricing { get; set; }
+    public ReservationViewModel Reservation { get; set; }
+    public ServiceViewModel Service { get; set; }
+}
