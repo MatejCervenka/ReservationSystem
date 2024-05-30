facf0cd84280ec03cde9c815707a511d219e81ca
MatejCervenka <127132225+MatejCervenka@users.noreply.github.com>
2024-05-30 17:25:42 +0200

    add: seperate index sections

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,5 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using ReserveSystem.Database;
 using ReserveSystem.Interfaces;
-using ReserveSystem.Models;
-using ReserveSystem.ViewModels;
-using ReserveSystem.Services; // Make sure you have this service
 
 namespace ReserveSystem.Controllers
 {
